354b2f318157e40488da107a81c631da306d409e
Raul Gonzalez <rgh0993@gmail.com>
2024-01-23 19:24:43 -0300

    terraform added

---
+++ homeworks/homework_1/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "zoomcamp-0"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = "zoomcamp-0-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
